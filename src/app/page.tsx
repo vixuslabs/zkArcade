ba74f45dcf003ecb67ae009ddf40f2a6c95647a4
0xtito <welovethepenguins@gmail.com>
2023-10-25 22:12:12 -0400

    prototyping pages and layouts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,80 @@
 import Link from "next/link";
 
-import { CreatePost } from "@/components/create-post";
-import { api } from "@/trpc/server";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from "@clerk/nextjs";
 
-export default async function Home() {
-  const hello = await api.post.hello.query({ text: "from tRPC" });
+export default function Home() {
+  // const hello = await api.post.hello.query({ text: "from tRPC" });
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#d34545] to-[#1ecddd] text-white">
+      <div className="container flex flex-col items-center justify-center gap-x-12 gap-y-6 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          Welcome to Hot <span className="text-[#1ecddd]">&apos;</span>
+          <span className="text-[#d34545]">n</span> Cold
         </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
+        <p className="text-xl">
+          Hide and seek with a twist. You hide in{" "}
+          <span className="underline underline-offset-2">their</span> room, from{" "}
+          <span className="underline underline-offset-2">your</span> room.
+        </p>
+        <div className="flex items-center justify-between gap-x-12">
+          <SignedIn>
+            <Link
+              className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm"
+              href="/dashboard"
+              prefetch
+            >
+              Dashboard
+            </Link>
+          </SignedIn>
 
-        <CrudShowcase />
+          <SignedOut>
+            <ClerkLoading>
+              <div className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm">
+                Sign in
+              </div>
+              <div className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm">
+                Sign up
+              </div>
+            </ClerkLoading>
+            <ClerkLoaded>
+              <SignInButton
+                // @ts-expect-error SignInButton can take className
+                className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm"
+                mode="modal"
+              />
+              <SignUpButton
+                // @ts-expect-error SignUpButton can take className
+                className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm"
+                mode="modal"
+              />
+            </ClerkLoaded>
+          </SignedOut>
+        </div>
       </div>
     </main>
   );
 }
 
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest.query();
+// async function CrudShowcase() {
+// const latestPost = await api.post.getLatest.query();
 
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
+//   return (
+//     <div className="w-full max-w-xs">
+//       {latestPost ? (
+//         <p className="truncate">Your most recent post: {latestPost.name}</p>
+//       ) : (
+//         <p>You have no posts yet.</p>
+//       )}
 
-      <CreatePost />
-    </div>
-  );
-}
+//       <CreatePost />
+//     </div>
+//   );
+// }
