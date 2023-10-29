02d6d737a6648a6dd025bd9e185d72907961d3b9
Rafael Campos <racampos@gmail.com>
2023-10-29 13:39:39 -0600

    Adds structs to deal with 3d objects. Adds a method to verify the validity of an object's position

--- mina/src/main.ts
+++ mina/src/main.ts
@@ -1,12 +1,11 @@
-import { HotnCold } from './HotnCold.js';
+import { HotnCold, Point, Object3D, Plane, Box, Room } from './HotnCold.js';
 import {
   Field,
   Mina,
   PrivateKey,
   AccountUpdate,
+  Poseidon
 } from 'o1js';
-
-console.log('o1js loaded');
 
 const useProof = false;
 
@@ -16,8 +15,8 @@ const { privateKey: deployerKey, publicKey: deployerAccount } =
   Local.testAccounts[0];
 const { privateKey: senderKey, publicKey: senderAccount } =
   Local.testAccounts[1];
-
-const salt = Field.random();
+  
+const object = Object3D.fromCenterAndRadius(Point.fromCoords(Field(50), Field(50), Field(50)), Field(25));
 
 // ----------------------------------------------------
 
@@ -29,26 +28,12 @@ const zkAppInstance = new HotnCold(zkAppAddress);
 const deployTxn = await Mina.transaction(deployerAccount, () => {
   AccountUpdate.fundNewAccount(deployerAccount);
   zkAppInstance.deploy();
-  zkAppInstance.initState(salt, Field(750));
+  zkAppInstance.commitObject(object);
 });
 await deployTxn.prove();
 await deployTxn.sign([deployerKey, zkAppPrivateKey]).send();
 
-// get the initial state of IncrementSecret after deployment
-const num0 = zkAppInstance.x.get();
-console.log('state after init:', num0.toString());
+const objectHash = zkAppInstance.objectHash.get();
+console.log('Commited on-chain hash:', objectHash);
+console.log('Computed object hash: ', Poseidon.hash([object.center.x, object.center.y, object.center.z, object.radius]))
 
-// ----------------------------------------------------
-
-const txn1 = await Mina.transaction(senderAccount, () => {
-  zkAppInstance.incrementSecret(salt, Field(750));
-});
-await txn1.prove();
-await txn1.sign([senderKey]).send();
-
-const num1 = zkAppInstance.x.get();
-console.log('state after txn1:', num1.toString());
-
-// ----------------------------------------------------
-
-console.log('Shutting down');
