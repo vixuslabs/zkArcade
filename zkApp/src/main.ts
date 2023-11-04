60971ab1d3a6e1008dedc479bbb8990b65c7d497
Rafael Campos <racampos@gmail.com>
2023-11-04 16:08:25 -0600

    Replaces the static scene data (boxes and objects) with values computed from real world data

--- zkApp/src/main.ts
+++ zkApp/src/main.ts
@@ -3,15 +3,52 @@
 /* eslint @typescript-eslint/no-unsafe-return: 0 */ 
 /* eslint @typescript-eslint/no-unsafe-member-access: 0 */
 
-import { Point, Object3D, Plane, Box, Room } from './structs.js';
+import { Point, Object3D, Plane, Box, Room, ThreeBox, ThreeObject } from './structs.js';
 import { HotnCold } from './HotnCold.js';
 import {
   Field,
   Mina,
   PrivateKey,
+  Struct,
   AccountUpdate,
   Poseidon
 } from 'o1js';
+import { boxes, planes, realWorldHiddenObject } from './scene.js' 
+import * as THREE from 'three';
+
+const o1jsBoxes: { o1jsBox: Box; o1jsObject: Object3D; }[] = [];
+boxes.forEach((box) => {
+  const vertices = new Float32Array(Object.values(box.vertices));
+  const matrix = new THREE.Matrix4();
+  matrix.fromArray(box.matrix);
+  const inverseMatrix = new THREE.Matrix4().copy(matrix).invert();
+  const hiddenObject = new THREE.Vector3(...realWorldHiddenObject.coords);
+  hiddenObject.applyMatrix4(inverseMatrix);
+
+  const translationToOrigin = new THREE.Vector3(vertices[0], vertices[1], vertices[2]);
+  translationToOrigin.negate(); // Translation to origin is the negative of the first vertex
+  const translationToOriginMatrix = new THREE.Matrix4().makeTranslation(translationToOrigin.x, translationToOrigin.y, translationToOrigin.z);
+  hiddenObject.applyMatrix4(translationToOriginMatrix);
+
+  const translationToPositiveCoords = new THREE.Vector3();
+  const objRadius = realWorldHiddenObject.radius;
+  translationToPositiveCoords.x = hiddenObject.x - objRadius < 0 ? -hiddenObject.x + objRadius : 0;
+  translationToPositiveCoords.y = hiddenObject.y - objRadius< 0 ? -hiddenObject.y + objRadius : 0;
+  translationToPositiveCoords.z = hiddenObject.z - objRadius < 0 ? -hiddenObject.z + objRadius : 0;
+  const translationToPositiveCoordsMatrix = new THREE.Matrix4().makeTranslation(translationToPositiveCoords.x, translationToPositiveCoords.y, translationToPositiveCoords.z);  
+  hiddenObject.applyMatrix4(translationToPositiveCoordsMatrix);
+
+  const translationMatrix = translationToOriginMatrix.clone().multiply(translationToPositiveCoordsMatrix);
+  const threeBox = new ThreeBox(vertices, translationMatrix);
+  const o1jsBox = threeBox.toO1jsBox();
+  const threeObject = new ThreeObject(hiddenObject, realWorldHiddenObject.radius);
+  const o1jsObject = threeObject.toO1jsObject();
+  o1jsBoxes.push({o1jsBox, o1jsObject});
+});
+
+const testObject: Object3D = o1jsBoxes[0].o1jsObject;
+const testBox: Box = o1jsBoxes[0].o1jsBox;
+
 
 const useProof = false;
 
@@ -22,14 +59,6 @@ const { privateKey: deployerKey, publicKey: deployerAccount } =
 const { privateKey: senderKey, publicKey: senderAccount } =
   Local.testAccounts[1];
   
-
-const objCoords = {
-  "x": 378085,
-  "y": 796820,
-  "z": 426573
-}
-const objRadius = Math.round(0.02 * 1000000);
-const object = Object3D.fromCenterAndRadius(Point.fromCoords(Field(objCoords.x), Field(objCoords.y), Field(objCoords.z)), Field(objRadius));
 
 // ----------------------------------------------------
 
@@ -52,24 +81,10 @@ const zkAppAddress = zkAppPrivateKey.toPublicKey();
 
 // ----------------------------------------------------
 
-const closestVertex = {
-  "x": 0,
-  "y": 0,
-  "z": 0
-}
-const farthestVertex = {
-  "x": 553329,
-  "y": 1042254,
-  "z": 721439
-}
-const a = Point.fromCoords(Field(closestVertex.x), Field(closestVertex.y), Field(closestVertex.z));
-const b = Point.fromCoords(Field(farthestVertex.x), Field(farthestVertex.y), Field(farthestVertex.z));
-const box = Box.fromPoints(a, b);
+testBox.assertIsOutside(testObject);
 
-box.assertIsOutside(object);
-
-const plane = Plane.fromPoints(Point.fromCoords(Field(0), Field(0), Field(0)), Point.fromCoords(Field(0), Field(0), Field(1)), Point.fromCoords(Field(1), Field(0), Field(0)), Point.fromCoords(Field(1), Field(0), Field(1)));
-const room = Room.fromPlanesAndBoxes([plane], [box]);
+// const plane = Plane.fromPoints(Point.fromCoords(Field(0), Field(0), Field(0)), Point.fromCoords(Field(0), Field(0), Field(1)), Point.fromCoords(Field(1), Field(0), Field(0)), Point.fromCoords(Field(1), Field(0), Field(1)));
+// const room = Room.fromPlanesAndBoxes([plane], [testBox]);
 // room.assertNoCollisions(object);
 
 // const txn1 = await Mina.transaction(senderAccount, () => {
--- zkApp/src/structs.ts
+++ zkApp/src/structs.ts
@@ -4,6 +4,7 @@
 /* eslint @typescript-eslint/no-unsafe-member-access: 0 */
 
 import { Field, Struct } from 'o1js';
+import * as THREE from 'three';
 
 export class Point extends Struct({ x: Field, y: Field, z: Field }) {
     static fromCoords(x: Field, y: Field, z: Field): Point {
@@ -11,74 +12,73 @@ export class Point extends Struct({ x: Field, y: Field, z: Field }) {
     }
   }
   
-  // An object is a sphere.
-  export class Object3D extends Struct({ center: Point, radius: Field }) {
+// An object is a sphere.
+export class Object3D extends Struct({ center: Point, radius: Field }) {
     static fromCenterAndRadius(center: Point, radius: Field): Object3D {
-      return new Object3D({ center, radius });
+        return new Object3D({ center, radius });
     }
-  
+
     // The object's bounding box.
     minX() {
-      return this.center.x.sub(this.radius);
+        return this.center.x.sub(this.radius);
     }
     minY() {
-      return this.center.y.sub(this.radius);
+        return this.center.y.sub(this.radius);
     }
     minZ() {
-      return this.center.z.sub(this.radius);
+        return this.center.z.sub(this.radius);
     }
     maxX() {
-      return this.center.x.add(this.radius);
+        return this.center.x.add(this.radius);
     }
     maxY() {
-      return this.center.y.add(this.radius);
+        return this.center.y.add(this.radius);
     }
     maxZ() {
-      return this.center.z.add(this.radius);
+        return this.center.z.add(this.radius);
     }
+}
   
-  }
-  
-  // A plane is defined by 4 points.
-  export class Plane extends Struct({ a: Point, b: Point, c: Point, d: Point }) {
+// A plane is defined by 4 points.
+export class Plane extends Struct({ a: Point, b: Point, c: Point, d: Point }) {
     static fromPoints(a: Point, b: Point, c: Point, d: Point) {
-      return new Plane ({ a, b, c, d });
+        return new Plane ({ a, b, c, d });
     }
-  }
+}
   
-  // A box is defined by 2 points 'a' and 'b'.
-  // 'a' is the min point (bottom, near, left) and 'b' is the max point (top, far, right).
-  export class Box extends Struct({ a: Point, b: Point }) {
+// A box is defined by 2 points 'a' and 'b'.
+// 'a' is the min point (bottom, near, left) and 'b' is the max point (top, far, right).
+export class Box extends Struct({ a: Point, b: Point }) {
     static fromPoints(a: Point, b: Point) {
-      return new Box({ a, b });
+        return new Box({ a, b });
     }
-  
+
     // Check that the object is outside the box.
     assertIsOutside(object: Object3D) {
-    
-      const minX = this.a.x;
-      const minY = this.a.y;
-      const minZ = this.a.z;
-  
-      const maxX = this.b.x;
-      const maxY = this.b.y;
-      const maxZ = this.b.z;
-  
-      object.maxX().lessThan(minX)
-      .or(object.maxY().lessThan(minY))
-      .or(object.maxZ().lessThan(minZ))
-      .or(object.minX().greaterThan(maxX))
-      .or(object.minY().greaterThan(maxY))
-      .or(object.minZ().greaterThan(maxZ))
-      .assertTrue();
+
+        const minX = this.a.x;
+        const minY = this.a.y;
+        const minZ = this.a.z;
+
+        const maxX = this.b.x;
+        const maxY = this.b.y;
+        const maxZ = this.b.z;
+
+        object.maxX().lessThan(minX)
+        .or(object.maxY().lessThan(minY))
+        .or(object.maxZ().lessThan(minZ))
+        .or(object.minX().greaterThan(maxX))
+        .or(object.minY().greaterThan(maxY))
+        .or(object.minZ().greaterThan(maxZ))
+        .assertTrue();
     }
+
+}
   
-  }
-  
-  // A room is defined by a list of planes and boxes.
-  export class Room extends Struct({ planes: [Plane], boxes: [Box] }) {
+// A room is defined by a list of planes and boxes.
+export class Room extends Struct({ planes: [Plane], boxes: [Box] }) {
     static fromPlanesAndBoxes(planes: [Plane], boxes: [Box]) {
-      return new Room({ planes, boxes });
+        return new Room({ planes, boxes });
     }
 
       // Check that the object is inside the room.
@@ -90,4 +90,69 @@ export class Point extends Struct({ x: Field, y: Field, z: Field }) {
       box.assertIsOutside(object);
     }
   }
+}
+
+// Representation of a box in 3D space using three.js types, which are used inside the
+// WebXR scene. The box is defined by 2 points 'a' and 'b'. 
+export class ThreeBox {
+    a: THREE.Vector3;
+    b: THREE.Vector3;
+    scale: number;
+
+    // The constructor takes the vertices of the box and a translation matrix to translate the box
+    // in such a way that its sides are aligned to the x, y, and z axes and all vertices are positive.
+    // This is so that the box can be represented with just two points, 'a' and 'b'.
+    // Optionally, it takes a scale factor to scale the box when exporting it to the o1js format.
+    constructor (vertices: Float32Array, translationMatrix: THREE.Matrix4, scale: number = 1000000) {
+        this.scale = scale;
+        const v = [];
+        for (let i = 0; i < vertices.length; i += 3) {
+            const vertex = new THREE.Vector3(vertices[i], vertices[i + 1], vertices[i + 2]);
+            v.push(vertex.applyMatrix4(translationMatrix));
+        }
+        let closestVertex = new THREE.Vector3();
+        let farthestVertex = new THREE.Vector3();
+        let closestDistance = Infinity; // A very large number to start with
+        let farthestDistance = 0; // Start with 0 for the farthest
+        // Loop through all vertices to find the closest and farthest
+        v.forEach(vertex => {
+            let distance = vertex.length(); // Get the distance from origin
+            // Check if this vertex is closer than the current closest
+            if (distance < closestDistance) {
+            closestDistance = distance;
+            closestVertex = vertex;
+            }
+            // Check if this vertex is farther than the current farthest
+            if (distance > farthestDistance) {
+            farthestDistance = distance;
+            farthestVertex = vertex;
+            }
+        });
+        this.a = closestVertex
+        this.b = farthestVertex
+        }
+
+    toO1jsBox() {
+        const a = Point.fromCoords(Field(Math.round(this.a.x * this.scale)), Field(Math.round(this.a.y * this.scale)), Field(Math.round(this.a.z * this.scale)));
+        const b = Point.fromCoords(Field(Math.round(this.b.x * this.scale)), Field(Math.round(this.b.y * this.scale)), Field(Math.round(this.b.z * this.scale)));
+        return Box.fromPoints(a, b);
+    }
+}
+
+export class ThreeObject {
+    center: THREE.Vector3;
+    radius: number;
+    scale: number;
+  
+    constructor (center: THREE.Vector3, radius: number, scale: number = 1000000) {
+        this.center = center;
+        this.radius = radius;
+        this.scale = scale;
+    }
+
+    toO1jsObject() {
+        const center = Point.fromCoords(Field(Math.round(this.center.x * this.scale)), Field(Math.round(this.center.y * this.scale)), Field(Math.round(this.center.z * this.scale)));
+        const radius = Field(Math.round(this.radius * this.scale));
+        return Object3D.fromCenterAndRadius(center, radius);
+    }
 }
