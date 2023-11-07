63454afd5689a0ca66dba8342ae3244e46a999bf
0xtito <welovethepenguins@gmail.com>
2023-11-07 11:26:31 -0500

    fixed signin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@ import Link from "next/link";
 import {
   ClerkLoaded,
   ClerkLoading,
-  SignedIn,
-  SignedOut,
   SignInButton,
   SignUpButton,
 } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#d34545] to-[#1ecddd] text-white">
       <div className="container flex flex-col items-center justify-center gap-x-12 gap-y-6 px-4 py-16 ">
@@ -23,7 +24,7 @@ export default function Home() {
           <span className="underline underline-offset-2">your</span> room.
         </p>
         <div className="flex items-center justify-between gap-x-12">
-          <SignedIn>
+          {user ? (
             <Link
               className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm"
               href="/dashboard"
@@ -31,32 +32,32 @@ export default function Home() {
             >
               Dashboard
             </Link>
-          </SignedIn>
-
-          <SignedOut>
-            <ClerkLoading>
-              <div className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm">
-                Sign in
-              </div>
-              <div className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm">
-                Sign up
-              </div>
-            </ClerkLoading>
-            <ClerkLoaded>
-              <SignInButton
-                // @ts-expect-error SignInButton can take className
-                className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm"
-                mode="modal"
-                redirectUrl="/dashboard"
-              />
-              <SignUpButton
-                // @ts-expect-error SignUpButton can take className
-                className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm"
-                mode="modal"
-                redirectUrl="/dashboard"
-              />
-            </ClerkLoaded>
-          </SignedOut>
+          ) : (
+            <>
+              <ClerkLoading>
+                <div className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm">
+                  Sign in
+                </div>
+                <div className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm">
+                  Sign up
+                </div>
+              </ClerkLoading>
+              <ClerkLoaded>
+                <SignInButton
+                  // @ts-expect-error SignInButton can take className
+                  className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm"
+                  mode="modal"
+                  redirectUrl="/dashboard"
+                />
+                <SignUpButton
+                  // @ts-expect-error SignUpButton can take className
+                  className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm"
+                  mode="modal"
+                  redirectUrl="/dashboard"
+                />
+              </ClerkLoaded>
+            </>
+          )}
         </div>
       </div>
     </main>
