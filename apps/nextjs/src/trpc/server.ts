e852a55d3770da7976dad8d4751c097472a50982
0xtito <welovethepenguins@gmail.com>
2023-12-05 17:28:56 -0500

    removed node-fetch + add encoding

--- apps/nextjs/src/trpc/server.ts
+++ apps/nextjs/src/trpc/server.ts
@@ -4,7 +4,7 @@ import {
   unstable_httpBatchStreamLink,
 } from "@trpc/client";
 import { headers } from "next/headers";
-import fetchPonyfill from "fetch-ponyfill";
+// import fetchPonyfill from "fetch-ponyfill";
 
 import type { AppRouter } from "@hot-n-cold/api";
 import { getUrl, transformer } from "./shared";
@@ -19,7 +19,7 @@ export const api = createTRPCProxyClient<AppRouter>({
     }),
     unstable_httpBatchStreamLink({
       url: getUrl(),
-      fetch: fetchPonyfill().fetch, // this line fixed mix match error
+      // fetch: fetchPonyfill().fetch, // this line fixed mix match error
       headers() {
         const heads = new Map(headers());
         heads.set("x-trpc-source", "rsc");
