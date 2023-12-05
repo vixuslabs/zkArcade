24b162b503c174ac69778a18f86d120086ac6907
0xtito <welovethepenguins@gmail.com>
2023-12-05 17:29:38 -0500

    updated drizzle config

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,9 +1,10 @@
-// import * as dotenv from "dotenv";
+import * as dotenv from "dotenv";
 import type { Config } from "drizzle-kit";
 
-// dotenv.config({
-//   path: "../../.env",
-// });
+dotenv.config({
+  path: "../../.env",
+});
+
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
