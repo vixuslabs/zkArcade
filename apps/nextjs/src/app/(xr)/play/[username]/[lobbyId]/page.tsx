af133401dcc1a4bccc5dcf87b406f2061b1ccff3
0xtito <welovethepenguins@gmail.com>
2023-12-22 12:40:10 -0500

    removed LobbyProvider (expect build to fail)

--- apps/nextjs/src/app/(xr)/play/[username]/[lobbyId]/page.tsx
+++ apps/nextjs/src/app/(xr)/play/[username]/[lobbyId]/page.tsx
@@ -1,15 +1,9 @@
 import { redirect } from "next/navigation";
-// import { LobbyProvider } from "@/components/client/providers";
-// import { Lobby } from "@/components/client/ui/lobby";
-import Lobby from "@/components/client/ui/lobby/LobbyZ";
+import { Lobby } from "@/components/client/ui/lobby";
 import { api } from "@/trpc/server";
 import { currentUser } from "@clerk/nextjs";
 
-async function LobbyPage({
-  params,
-}: {
-  params: { username: string; lobbyId: string };
-}) {
+async function LobbyPage() {
   const clerkUser = await currentUser();
 
   if (!clerkUser) redirect("/");
@@ -18,19 +12,7 @@ async function LobbyPage({
 
   if (!user) redirect("/");
 
-  return (
-    // <LobbyProvider
-    //   user={{
-    //     username: user.username,
-    //     imageUrl: user.image_url,
-    //     host: params.username === user.username,
-    //     ready: false,
-    //   }}
-    //   lobbyId={params.lobbyId}
-    // >
-    <Lobby />
-    // </LobbyProvider>
-  );
+  return <Lobby />;
 }
 
 export default LobbyPage;
