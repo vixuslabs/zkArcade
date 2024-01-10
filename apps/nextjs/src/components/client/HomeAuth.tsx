bdb07bb57bc3c53d473c63136b4ae42d92b0803a
0xtito <welovethepenguins@gmail.com>
2024-01-10 15:58:14 -0500

    create comp for home sign in/sign up buttons

---
+++ apps/nextjs/src/components/client/HomeAuth.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React from "react";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignInButton,
+  SignUpButton,
+  // useClerk
+} from "@clerk/nextjs";
+
+export function HomeAuth() {
+  // const { openSignIn } = useClerk();
+  return (
+    <>
+      <ClerkLoading>
+        <div className="rounded-md bg-secondary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm">
+          Sign in
+        </div>
+        <div className="rounded-md bg-secondary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm">
+          Sign up
+        </div>
+      </ClerkLoading>
+      <ClerkLoaded>
+        <SignInButton
+          // @ts-expect-error SignInButton can take className
+          className="rounded-md bg-secondary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm"
+          mode="modal"
+          //   redirectUrl="/dashboard"
+          afterSignInUrl="/dashboard"
+        />
+        <SignUpButton
+          // @ts-expect-error SignUpButton can take className
+          className="rounded-md bg-secondary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm"
+          mode="modal"
+          //   redirectUrl="/dashboard"
+          afterSignUpUrl="/dashboard"
+        />
+      </ClerkLoaded>
+    </>
+  );
+}
