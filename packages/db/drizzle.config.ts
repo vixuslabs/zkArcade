2b85554192724d8cdc6e123e66a9fdb3b0f9d18a
0xtito <welovethepenguins@gmail.com>
2024-01-14 20:07:51 -0500

    improved scripts for dev and prod scripts for drizzle studio

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,12 +1,6 @@
-import * as dotenv from "dotenv";
 import type { Config } from "drizzle-kit";
 
-dotenv.config({
-  path: "../../.env.production",
-});
-
 const uri = process.env.DATABASE_URL;
-// const uri = process.env.DEV_DATABASE_URL;
 
 if (!uri) {
   throw new Error("Missing database uri");
