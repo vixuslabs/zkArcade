f4e7554c043ab3f4e67a6538a87488a4f63a7082
0xtito <welovethepenguins@gmail.com>
2024-01-14 20:35:20 -0500

    shared getUrl function

--- apps/nextjs/src/trpc/shared.ts
+++ apps/nextjs/src/trpc/shared.ts
@@ -1,12 +1,16 @@
 import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
-import type { AppRouter } from "@zkarcade/api";
 import superjson from "superjson";
+
+import type { AppRouter } from "@zkarcade/api";
 
 export const transformer = superjson;
 
 function getBaseUrl() {
   if (typeof window !== "undefined") return "";
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  if (process.env.ENV === "production" && process.env.VERCEL_URL)
+    return `https://${process.env.VERCEL_URL}`;
+  if (process.env.ENV === "preview" && process.env.VERCEL_BRANCH_URL)
+    return `https://${process.env.VERCEL_BRANCH_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
   // return `https://funny-unlikely-dolphin.ngrok-free.app`;
 }
--- apps/nextjs/src/utils/trpc.ts
+++ apps/nextjs/src/utils/trpc.ts
@@ -2,15 +2,18 @@
 import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+
 import type { AppRouter } from "@zkarcade/api";
 import { transformer } from "@zkarcade/api";
 
-const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+import { getUrl } from "../trpc/shared";
 
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-};
+// const getBaseUrl = () => {
+//   if (typeof window !== "undefined") return ""; // browser should use relative url
+//   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+
+//   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+// };
 
 export const trpc = createTRPCNext<AppRouter>({
   config() {
@@ -23,7 +26,7 @@ export const trpc = createTRPCNext<AppRouter>({
             (opts.direction === "down" && opts.result instanceof Error),
         }),
         httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
+          url: getUrl(),
         }),
       ],
     };
