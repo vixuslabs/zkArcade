815922a176c17363d46cc388ec2354b3ffa43b6b
0xtito <welovethepenguins@gmail.com>
2024-01-14 20:15:07 -0500

    rm noti & friends channel providers

--- apps/nextjs/src/components/client/providers/index.ts
+++ apps/nextjs/src/components/client/providers/index.ts
@@ -1,5 +1,4 @@
 export { default as ThemeProvider } from "./ThemeProvider";
 export { default as DashboardTabProvider } from "./DashboardTabProvider";
 export { default as ControllerStateProvider } from "./ControllerStateProvider";
-export { default as FriendsChannelProvider } from "./FriendsChannelProvider";
 export { default as MinaProvider } from "./MinaProvider";
