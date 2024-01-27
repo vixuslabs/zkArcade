1a2ec41fb66bfdb0a06349b4cc594f01e84065f2
0xtito <welovethepenguins@gmail.com>
2024-01-27 11:39:47 -0600

    change AddFriend Button to 'Search'

--- apps/nextjs/src/components/client/dashboard/AddFriend.tsx
+++ apps/nextjs/src/components/client/dashboard/AddFriend.tsx
@@ -207,7 +207,7 @@ function AddFriend() {
                     Searching...
                   </>
                 ) : (
-                  "Add Friend"
+                  "Search"
                 )}
               </Button>
             </form>
