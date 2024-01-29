998eccdd6ac87ed9f357795bfae6a76b845e072d
0xtito <welovethepenguins@gmail.com>
2024-01-28 18:49:51 -0600

    added static imports to carousel

---
+++ apps/nextjs/src/components/client/dashboard/images/HotnColdImage.tsx
@@ -0,0 +1,19 @@
+// "use client"
+
+// import React from "react";
+// import Image from "next/image";
+
+// function CarouselImage() {
+//   return (
+//     <Image
+//       priority={altText === "Hot 'n Cold" ? true : false}
+//       width={width}
+//       height={height}
+//       src={imageUrl}
+//       alt={altText}
+//       placeholder="blur"
+//     />
+//   );
+// }
+
+// export default CarouselImage;
