bfffe9abc44f3e6dbcf4e5c5fdbbb4db3a26b8f5
Rafael Campos <racampos@gmail.com>
2024-02-16 11:23:46 -0600

    Refactors the provable code so it uses the 'zk3d' library instead of the legacy custom classes originally created for dealing with 3d operations.

--- packages/mina/src/main.ts
+++ packages/mina/src/main.ts
@@ -1,14 +1,12 @@
-import { Int64, verify } from "o1js";
+import { verify } from "o1js";
 import { boxes, planes, realWorldHiddenObject } from "./scene";
 import {
-  AffineTransformationMatrix,
   Box,
   Object3D,
   Plane,
   Room,
-  SCALE,
-  Vector3,
 } from "./structs";
+import { Vector3, Real64, Matrix4 } from 'zk3d';
 import { ValidateRoom, RoomAndObjectCommitment } from "./zkprogram";
 
 // Import the hidden object coordinates
@@ -18,44 +16,33 @@ if (
   realWorldHiddenObject.coords[2] !== undefined
 ) {
   const objectVector = new Vector3({
-    x: Int64.from(Math.round(realWorldHiddenObject.coords[0] * SCALE)),
-    y: Int64.from(Math.round(realWorldHiddenObject.coords[1] * SCALE)),
-    z: Int64.from(Math.round(realWorldHiddenObject.coords[2] * SCALE)),
+    x: Real64.from(realWorldHiddenObject.coords[0]),
+    y: Real64.from(realWorldHiddenObject.coords[1]),
+    z: Real64.from(realWorldHiddenObject.coords[2]),
   });
-  const objectRadius = Int64.from(
-    Math.round(realWorldHiddenObject.radius * SCALE),
-  );
+  const objectRadius = Real64.from(realWorldHiddenObject.radius);
+
   const object = Object3D.fromPointAndRadius(objectVector, objectRadius);
 
   const sceneBoxes: Box[] = [];
   boxes.forEach((b) => {
     const vertices = Object.values(b.vertices);
-    // Scale the original box so that all its vertices are integers
-    const scaledVertices = vertices.map((v) => {
-      if (typeof v === "number") {
-        return Math.round(v * SCALE);
-      }
-      // if v is not a number, return 0
-      return 0;
-    });
-    // Create an array of 8 Vector3 objects from the scaled vertices
+    // Create an array of 8 Vector3 objects from the vertices
     const vertexPoints: Vector3[] = [];
-    for (let i = 0; i < scaledVertices.length; i += 3) {
+    for (let i = 0; i < vertices.length; i += 3) {
       vertexPoints.push(
         new Vector3({
-          x: Int64.from(scaledVertices[i]!),
-          y: Int64.from(scaledVertices[i + 1]!),
-          z: Int64.from(scaledVertices[i + 2]!),
+          x: Real64.from(vertices[i]!),
+          y: Real64.from(vertices[i + 1]!),
+          z: Real64.from(vertices[i + 2]!),
         }),
       );
     }
-    // Scale the matrix elements and set the last element to 1 to keep it affine
-    const matrixElements = b.matrix.map((x) => Math.round(x * SCALE));
-    matrixElements[15] = 1;
-    // Instantiate the box from the vertices and the matrix
-    const box = Box.fromVertexPointsAndATM(
+    // Get the matrix elements
+    const matrixElements = b.matrix.map((x) => Real64.from(x));
+    const box = Box.fromVertexPointsAndMatrix(
       vertexPoints,
-      AffineTransformationMatrix.fromElements(matrixElements),
+      Matrix4.fromElements(matrixElements),
     );
     sceneBoxes.push(box);
   });
@@ -63,26 +50,24 @@ if (
   const scenePlanes: Plane[] = [];
   planes.forEach((p) => {
     const vertices = Object.values(p.position);
-    // Scale the original plane so that all its vertices are integers
-    const scaledVertices = vertices.map((v) => Math.round(v * SCALE));
-    // Create an array of 4 Vector3 objects from the scaled vertices
+    // Create an array of 4 Vector3 objects from the vertices
     const vertexPoints: Vector3[] = [];
-    for (let i = 0; i < scaledVertices.length; i += 3) {
+    for (let i = 0; i < vertices.length; i += 3) {
       vertexPoints.push(
         new Vector3({
-          x: Int64.from(scaledVertices[i]!),
-          y: Int64.from(scaledVertices[i + 1]!),
-          z: Int64.from(scaledVertices[i + 2]!),
+          x: Real64.from(vertices[i]!),
+          y: Real64.from(vertices[i + 1]!),
+          z: Real64.from(vertices[i + 2]!),
         }),
       );
     }
     // Scale the matrix elements and set the last element to 1 to keep it affine
-    const matrixElements = p.matrix.map((x) => Math.round(x * SCALE));
-    matrixElements[15] = 1;
+    const matrixElements = p.matrix.map((x) => Real64.from(x));
+    matrixElements[15] = Real64.from(1);
     // Instantiate the plane from the vertices and the matrix
-    const plane = Plane.fromVertexPointsAndATM(
+    const plane = Plane.fromVertexPointsAndMatrix(
       vertexPoints,
-      AffineTransformationMatrix.fromElements(matrixElements),
+      Matrix4.fromElements(matrixElements),
     );
     scenePlanes.push(plane);
   });
