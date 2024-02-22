e5650bf5ed1d8c8b406215c1ff20a3e54688d444
0xtito <welovethepenguins@gmail.com>
2024-02-22 16:43:06 -0500

    moved old landingPage to new, only temp

---
+++ apps/nextjs/src/app/new/page.tsx
@@ -0,0 +1,56 @@
+import { Suspense } from "react";
+import { unstable_noStore } from "next/cache";
+import Link from "next/link";
+import { HomeAuth } from "@/components/client/HomeAuth";
+import { currentUser } from "@clerk/nextjs";
+
+// export const dynamic = "force-dynamic";
+
+export default async function Home() {
+  unstable_noStore();
+  const user = await currentUser();
+
+  return (
+    <section className="flex min-h-screen w-full flex-col items-center justify-center py-12  md:py-24 lg:py-32 xl:py-48">
+      <div className="container px-4 md:px-6">
+        <div className="flex flex-col items-center space-y-4 text-center">
+          <div className="space-y-2">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+              zkArcade
+            </h1>
+            <p className="mx-auto max-w-[700px] md:text-xl">
+              your home for XR zero knowledge games, built on{" "}
+              <a
+                href="https://minaprotocol.com/"
+                rel="noreferrer"
+                target="_blank"
+                className="hover:underline hover:underline-offset-2"
+              >
+                Mina
+              </a>
+            </p>
+          </div>
+          <div className="w-full max-w-sm space-y-2">
+            <div className="flex justify-center space-x-4">
+              {user ? (
+                <Link
+                  className="rounded-md bg-secondary px-3.5 py-2.5 text-sm font-semibold shadow-sm"
+                  href="/dashboard"
+                  prefetch={false}
+                >
+                  Dashboard
+                </Link>
+              ) : (
+                <>
+                  <Suspense>
+                    <HomeAuth />
+                  </Suspense>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
