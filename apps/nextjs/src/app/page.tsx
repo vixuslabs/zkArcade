855cb90c726a4924911853f462fb4fbe1384d022
0xtito <welovethepenguins@gmail.com>
2024-02-29 20:20:32 -0500

    clean up commented out code

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -34,32 +34,6 @@ export default async function Page() {
         <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
           <div className="px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-32">
             <HeroSection />
-            {/* <div className="mx-auto max-w-2xl lg:mx-0">
-              <div className="px-8 py-20 text-left">
-                <h1 className="mt-24 text-4xl font-bold tracking-tight text-primary sm:mt-10 sm:text-6xl">
-                  Compete with the confidence every game is fair
-                </h1>
-                <p className="mt-6 text-lg leading-8">
-                  A collection of mixed reality multiplayer games, built on{" "}
-                  <a
-                    href="https://minaprotocol.com/"
-                    rel="noreferrer"
-                    target="_blank"
-                    className="underline underline-offset-2"
-                  >
-                    Mina
-                  </a>{" "}
-                  to help make cheating a thing of the past.
-                </p>
-                <div className="mt-10 flex items-center gap-x-6 sm:justify-center md:justify-start">
-                  <Button>Get Started</Button>
-
-                  <Button asChild variant="secondary">
-                    <Link href="/sandbox">Try Sandbox Mode</Link>
-                  </Button>
-                </div>
-              </div>
-            </div> */}
           </div>
         </div>
       </div>
